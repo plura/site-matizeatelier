a825f4763e8a52260db705014469dc25d2540eec
trpsimoes <trpsimoes@gmail.com>
2026-04-19 23:59:15 +0100

    services: wrap in single section, articles per service, unified h2 size (xl)

--- theme/page-services.php
+++ theme/page-services.php
@@ -17,47 +17,49 @@ $services = new WP_Query( [
 <main id="main" class="site-main page-services">
 
 	<?php if ( $services->have_posts() ) : ?>
-	<div class="container">
-		<?php while ( $services->have_posts() ) : $services->the_post(); ?>
+	<section class="services-section">
+		<div class="container">
+			<?php while ( $services->have_posts() ) : $services->the_post(); ?>
 
-		<?php
-		$tagline     = get_field( 'mtz_service_tagline' );
-		$description = get_field( 'mtz_service_description' );
-		$images      = get_field( 'mtz_service_gallery' ) ?: [];
-		$thumb_id    = get_post_thumbnail_id();
-		?>
+			<?php
+			$tagline     = get_field( 'mtz_service_tagline' );
+			$description = get_field( 'mtz_service_description' );
+			$images      = get_field( 'mtz_service_gallery' ) ?: [];
+			$thumb_id    = get_post_thumbnail_id();
+			?>
 
-		<section class="content-section service-section">
-			<div class="content-section__inner">
+			<article class="content-section service-section">
+				<div class="content-section__inner">
 
-				<?php if ( $thumb_id ) : ?>
-				<div class="content-section__media service-section__image">
-					<?php echo plura_wp_image( $thumb_id, 'large', [ 'class' => 'service-section__img' ] ); ?>
+					<?php if ( $thumb_id ) : ?>
+					<div class="content-section__media service-section__image">
+						<?php echo plura_wp_image( $thumb_id, 'large', [ 'class' => 'service-section__img' ] ); ?>
+					</div>
+					<?php endif; ?>
+
+					<div class="service-section__content">
+						<h2 class="service-section__title"><?php the_title(); ?></h2>
+
+						<?php if ( $tagline ) : ?>
+						<p class="service-section__tagline"><?php echo esc_html( $tagline ); ?></p>
+						<?php endif; ?>
+
+						<?php if ( $description ) : ?>
+						<div class="service-section__description"><?php echo wp_kses_post( $description ); ?></div>
+						<?php endif; ?>
+
+						<?php $cluster = mtz_gallery_cluster( $images ); ?>
+						<?php if ( $cluster ) : ?>
+						<div class="service-section__cluster"><?php echo $cluster; ?></div>
+						<?php endif; ?>
+					</div>
+
 				</div>
-				<?php endif; ?>
+			</article>
 
-				<div class="service-section__content">
-					<h2 class="service-section__title"><?php the_title(); ?></h2>
-
-					<?php if ( $tagline ) : ?>
-					<p class="service-section__tagline"><?php echo esc_html( $tagline ); ?></p>
-					<?php endif; ?>
-
-					<?php if ( $description ) : ?>
-					<div class="service-section__description"><?php echo wp_kses_post( $description ); ?></div>
-					<?php endif; ?>
-
-					<?php $cluster = mtz_gallery_cluster( $images ); ?>
-					<?php if ( $cluster ) : ?>
-					<div class="service-section__cluster"><?php echo $cluster; ?></div>
-					<?php endif; ?>
-				</div>
-
-			</div>
-		</section>
-
-		<?php endwhile; wp_reset_postdata(); ?>
-	</div>
+			<?php endwhile; wp_reset_postdata(); ?>
+		</div>
+	</section>
 	<?php endif; ?>
 
 	<?php get_template_part( 'template-parts/brands' ); ?>
