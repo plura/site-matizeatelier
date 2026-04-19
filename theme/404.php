6a7070367afd6cc5877c046a2573f0b82461c8e4
trpsimoes <trpsimoes@gmail.com>
2026-04-19 19:05:14 +0100

    404: change strings to English

--- theme/404.php
+++ theme/404.php
@@ -4,10 +4,10 @@
 
 	<section class="error-404">
 		<div class="error-404__inner">
-			<h1 class="error-404__heading"><?php esc_html_e( 'Página não encontrada', 'matize' ); ?></h1>
-			<p class="error-404__text"><?php esc_html_e( 'A página que procura não existe ou foi removida.', 'matize' ); ?></p>
+			<h1 class="error-404__heading"><?php esc_html_e( 'Page not found', 'matize' ); ?></h1>
+			<p class="error-404__text"><?php esc_html_e( 'The page you are looking for does not exist or has been removed.', 'matize' ); ?></p>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="error-404__link">
-				<?php esc_html_e( 'Voltar ao início', 'matize' ); ?>
+				<?php esc_html_e( 'Back to home', 'matize' ); ?>
 			</a>
 		</div>
 	</section>
