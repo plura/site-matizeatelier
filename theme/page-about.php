aaac0ac1a02a8367a44c7e4a9a3e412c85f2c570
trpsimoes <trpsimoes@gmail.com>
2026-04-20 00:12:46 +0100

    typography: remove font-size overrides, let h1/h2 base scale apply (3xl/2xl)

--- theme/page-about.php
+++ theme/page-about.php
@@ -26,7 +26,7 @@ get_template_part( 'template-parts/page-header' ); ?>
 			<div class="content-section__inner">
 
 				<div class="about-section__text">
-					<h2 class="about-section__title"><?php echo esc_html( $section['title'] ); ?></h2>
+					<h2 class="about-section__title section-header__title"><?php echo esc_html( $section['title'] ); ?></h2>
 					<?php echo wp_kses_post( $text ); ?>
 				</div>
 
